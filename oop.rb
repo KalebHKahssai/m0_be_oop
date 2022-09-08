2d71efad12b248aec39420813ed57389dce374dc
Kaleb Kahssai <kalebkahssai@gmail.com>
2022-09-08 10:17:33 -0700

    Initial commit

--- oop.rb
+++ oop.rb
@@ -3,6 +3,20 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+ class Unicorn
+   def initialize(name)
+     @name = name
+     @color = "silver"
+  end
+
+  def say(message)
+    p "*~*#{message}*~*"
+  end
+end
+
+unicornk = Unicorn.new("Voldemort")
+p unicornk
+p unicornk.say ("His name is Voldemort")
 
 
 #  Write a class called Vampire
@@ -11,6 +25,30 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+  def initialize(name)
+    @name = name
+    @pet = "bat"
+    @thirsty = true
+  end
+
+  def change_pet(new_pet)
+    @pet = new_pet
+  end
+
+  def drink
+    @thirsty = false
+  end
+end
+
+chocula = Vampire.new("Count")
+p chocula
+
+chocula.change_pet("eagle")
+p chocula
+
+p chocula.drink
+
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +56,31 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+  def initialize(name, rider, color)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = true
+  end
+
+  def eat (food)
+    if food >= 4
+      @is_hungry = false
+    end
+  end
+end
+
+
+drag1 = Dragon.new("Shenron", "Eragon", "Maleficent")
+p drag1
+
+drag1.eat(2)
+p drag1
+
+drag1.eat(8)
+p drag1
 
 
 #  Write a Hobbit class
@@ -28,3 +91,41 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+  def initialize(name, disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @its_adult = false
+    @its_old = false
+    @has_ring = false
+
+    if @name == "Frodo"
+      @has_ring = true
+    else
+      @has_ring = false
+    end
+
+    def change_age(age_new)
+      @age = age_new
+      if age_new >= 33
+        @is_adult = true
+      end
+
+      if age_new >= 101
+        @is_old = true
+      end
+
+      def celebrate_birthday
+        p "It's your #{@age +1} year hobbit!"
+      end
+    end
+    end
+end
+
+hobbit007 = Hobbit.new("Frodo", "kind")
+
+hobbit007.change_age(102)
+p hobbit007
+p hobbit007.celebrate_birthday
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,15 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+  + This final CFU was definitely the most challenging and daunting, I felt weary doing outside research because I didn't know if it was a "good resource" or even at times what to look up. I felt I need to get better on how to learn using outside resources outside of my small group. I gave it my best shot, but I definitely need to do more reviewing and practice. I was a bit taken back how much "stuff" I felt was not included in the lesson, had to figure out, but at the same time realized that was with intention.
+
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+  + Since I’ve started mod0, I’ve been using the Pomodoro Technique which I’ve seen good results from. I’ve been doing 60/15 blocks, every hour I take a 15 minute break where I step away from the computer to take a walk outside.
+
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+ +  I think I will need to preview intense technical lessons and challenges like this ahead of time so I am not feeling overwhelmed. More importantly, I need to improve on how I can connect and visualize concepts together. Especially, as we go into more complex problems and concepts.
